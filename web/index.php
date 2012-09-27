b5e385f64daa21a20ccd5b714463b772a4e97411
Albertino Silva <albertino05@gmail.com>
2012-09-28 01:55:09 +0200

    Final 2

--- web/index.php
+++ web/index.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once 'kint/Kint.class.php';
+/*include_once 'kint/Kint.class.php';*/
 require_once __DIR__ . '/../autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
@@ -11,18 +11,7 @@ $request = Request::createFromGlobals();
 
 $input = $request->get('name', 'World');
 
-
 $response = new Response(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
-
-/**
- * SOME EXAMPLES
- */
-$response->setContent('Hello world! - faked');
-$response->setStatusCode(200);
-$response->headers->set('Content-Type', 'text/html');
-
-// configure the HTTP cache headers
-$response->setMaxAge(10000000);
 
 $response->send();
 ?>
