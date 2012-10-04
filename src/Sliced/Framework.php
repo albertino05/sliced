23240d93a4cd5d3b429bfa44c49deee1dc24a3e5
Albertino Silva <albertino05@gmail.com>
2012-10-04 18:42:16 +0200

    http-kernel working good

--- src/Sliced/Framework.php
+++ src/Sliced/Framework.php
@@ -1,43 +1,45 @@
 <?php
 
-    // src/Sliced/Framework.php
+// src/Sliced/Framework.php
 
-    namespace Sliced;
+namespace Sliced;
 
-    use Symfony\Component\HttpFoundation\Request;
-    use Symfony\Component\HttpFoundation\Response;
-    use Symfony\Component\Routing\Matcher\UrlMatcher;
-    use Symfony\Component\Routing\Exception\ResourceNotFoundException;
-    use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
 
-    class Framework
-    {
-        protected $matcher;
-        protected $resolver;
+class Framework
+{
 
-        public function __construct(UrlMatcher $matcher, ControllerResolver $resolver)
-        {
-            $this->matcher = $matcher;
-            $this->resolver = $resolver;
-        }
+      protected $matcher;
+      protected $resolver;
 
-        public function handle(Request $request)
-        {
-            $this->matcher->getContext()->fromRequest($request);
+      public function __construct(UrlMatcher $matcher, ControllerResolver $resolver)
+      {
+	  $this->matcher = $matcher;
+	  $this->resolver = $resolver;
+      }
 
-            try {
-                $request->attributes->add($this->matcher->match($request->getPathInfo()));
+      public function handle(Request $request)
+      {
+	  $this->matcher->getContext()->fromRequest($request);
 
-                $controller = $this->resolver->getController($request);
-                $arguments = $this->resolver->getArguments($request, $controller);
+	  try {
+	        $request->attributes->add($this->matcher->match($request->getPathInfo()));
 
-                return call_user_func_array($controller, $arguments);
-            } catch (ResourceNotFoundException $e) {
-                return new Response('Not Found', 404);
-            } catch (\Exception $e) {
-	        d($e);
-                return new Response('An error occurred', 500);
-            }
-        }
-    }
+	        $controller = $this->resolver->getController($request);
+	        $arguments = $this->resolver->getArguments($request, $controller);
+
+	        return call_user_func_array($controller, $arguments);
+	  } catch (ResourceNotFoundException $e) {
+	        return new Response('Not Found', 404);
+	  } catch (\Exception $e) {
+	        return new Response('An error occurred', 500);
+	  }
+      }
+
+}
+
 ?>
